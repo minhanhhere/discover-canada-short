1a3e119b73b05314b382d6e58f89453def26cefd
Martin Tran <matran@sgi.sk.ca>
2026-01-26 21:36:47 -0600

    remove

--- website/assets/js/a7456010.6b3292a2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkdiscover_canada=globalThis.webpackChunkdiscover_canada||[]).push([[235],{8552(a){a.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- website/assets/js/aba21aa0.83b8d1fb.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkdiscover_canada=globalThis.webpackChunkdiscover_canada||[]).push([[742],{7093(a){a.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
