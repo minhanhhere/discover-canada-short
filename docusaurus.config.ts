bb3f5cf854e548a53c95727561ce8b809c55d890
Martin Tran <matran@sgi.sk.ca>
2026-01-26 21:59:50 -0600

    update Edit This Page url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/minhanhhere/discover-canada-short/tree/main/docs/',
+            'https://github.com/minhanhhere/discover-canada-short/tree/main/',
         },
         blog: false,
       } satisfies Preset.Options,
