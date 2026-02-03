185142b8f21ac6656dc655265b22d4f378395ef1
Martin Tran <matran@sgi.sk.ca>
2026-02-03 09:37:57 -0600

    setup docusaurus url via vercel.json

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://discover-canada.vercel.app",
+  url: process.env.DOCUSAURUS_URL || "https://minhanhhere.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: process.env.DOCUSAURUS_BASE_URL || "/discover-canada-short/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
