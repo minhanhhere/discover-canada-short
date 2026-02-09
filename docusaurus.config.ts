5f8e0f9acec2ec8708a85b1b12f6773f52cfa5b4
Martin Tran <matran@sgi.sk.ca>
2026-02-09 17:41:18 -0600

    fix pdf url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,7 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -114,7 +115,7 @@ const config: Config = {
           items: [
             {
               label: "Discover Canada Summary (PDF)",
-              href: "data/DiscoverCanadaSummary.pdf",
+              href: useBaseUrl("data/DiscoverCanadaSummary.pdf"),
             },
             {
               label: "Discover Canada (PDF)",
